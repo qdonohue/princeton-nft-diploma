180a165b43c566c7c1a7f6d028d736668e64b0c0
Quinn Donohue <quinndonohue@gmail.com>
2022-04-30 16:28:55 -0400

    Functioning image upload on NFT card

--- components/registration/ImageUpload.tsx
+++ components/registration/ImageUpload.tsx
@@ -4,17 +4,15 @@ import { useDropzone } from "react-dropzone";
 
 const ImageUpload = ({
   setImageData,
-  resetImage,
-  imageURL,
 }: {
-  setImageData: (arg0: File, arg1: string) => void;
-  resetImage: () => void;
-  imageURL: string;
+  setImageData: (arg0: File) => void;
 }) => {
+  const [imgUrl, setImgUrl] = useState("/default.jpeg");
   const onDrop = useCallback(async (acceptedFiles) => {
+    console.log(acceptedFiles[0]);
     const imgLocalURL = URL.createObjectURL(acceptedFiles[0]);
-
-    setImageData(acceptedFiles[0], imgLocalURL);
+    setImgUrl(imgLocalURL);
+    setImageData(acceptedFiles[0]);
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -25,31 +23,33 @@ const ImageUpload = ({
   });
 
   return (
-    <div
-      {...getRootProps({
-        className:
-          "w-48 h-48 mx-auto my-auto bg-slate-200 border border-dashed rounded-2xl border-black flex flex-col items-center hover:bg-slate-300 hover:shadow-xl",
-      })}
-    >
+    <div {...getRootProps({})}>
       <input {...getInputProps()} />
-      <svg
-        className="mx-auto my-auto h-2/3 w-2/3 text-black"
-        stroke="currentColor"
-        fill="none"
-        viewBox="0 0 48 48"
-        aria-hidden="true"
-      >
-        <path
-          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-          strokeWidth={2}
-          strokeLinecap="round"
-          strokeLinejoin="round"
+      <div className="relative shadow-sm border border-slate-100 rounded max-w-fit max-h-fit overflow-hidden">
+        <img
+          className="relative inset-0 object-cover z-0 w-4/5 h-4/5 mx-auto my-auto"
+          src={imgUrl}
         />
-      </svg>
-      <div className="text-sm font-light">
-        {isDragActive ? "Drop your file here" : "Upload your own image"}
+        <div className="opacity-0 flex flex-col justify-center hover:opacity-70 hover:bg-slate-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xl text-slate-800 font-semibold hover:cursor-pointer">
+          <svg
+            className="h-2/3 w-2/3"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 48 48"
+            aria-hidden="true"
+          >
+            <path
+              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <div className="text-sm -mt-2">
+            {isDragActive ? "Drop image here" : "Click or drag to change image"}
+          </div>
+        </div>
       </div>
-      <div className="text-xs font-light pb-2">(png or jpeg)</div>
     </div>
   );
 };
--- components/registration/NFTPreview.tsx
+++ components/registration/NFTPreview.tsx
@@ -1,21 +1,12 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import ImageUpload from "./ImageUpload";
 
 export type NftType = {
   name?: string;
   image?: string | ImageData;
   description?: string;
   classYear?: Number;
-};
-
-const ClickableImage = ({ source }: { source: string }) => {
-  return (
-    <div className="relative my-2 shadow-sm border border-slate-100 rounded-sm">
-      <img className="relative inset-0 object-cover z-0" src="/nassau.jpeg" />
-      <div className="opacity-0 hover:opacity-70 hover:bg-slate-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xl text-slate-800 font-semibold hover:cursor-pointer">
-        Change Image
-      </div>
-    </div>
-  );
 };
 
 const Label = ({ title }: { title: string }) => (
@@ -34,53 +25,61 @@ export const NFTPreview = ({
   classYear,
 }: NftType) => {
   const { register, handleSubmit } = useForm();
+  const [imageData, setImageData] = useState<File>();
 
   const onSubmit: any = (data: any) => {
     console.log(data);
   };
 
+  const changeImage = (f: File) => {
+    setImageData(f);
+  };
+
   return (
-    <form
-      className="w-fit border rounded-lg border-slate-500 m-2 p-2"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="text-xl">NFT preview</div>
-      <div className="text-sm font-light">
-        All values will be publically visible on your NFT.
+    <>
+      <div className="w-fit border rounded-lg border-slate-500 m-2 p-2">
+        <div className="text-xl">NFT preview</div>
+        <div className="text-sm font-light">
+          All values will be publically visible on your NFT.
+        </div>
+        <hr className="pb-1" />
+        <ImageUpload setImageData={changeImage} />
+        <div className="text-xs font-light text-center text-slate-500">
+          If you don't like our image, feel free to upload your own
+        </div>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="pl-2 divide-y divide-slate-300 space-y-2">
+            <div className={rowStyling}>
+              <Label title={"Name"} />
+              <input
+                type={"text"}
+                id="name"
+                {...register("name")}
+                className={inputStyling}
+              />
+            </div>
+            <div className={rowStyling + " flex-col"}>
+              <Label title={"Description"} />
+              <textarea
+                placeholder="A description for your NFT diploma"
+                rows={4}
+                cols={50}
+                id="name"
+                {...register("description")}
+                className={inputStyling + " p-2 mt-2 mr-2 text-sm ml-0"}
+              />
+            </div>
+            <div className="w-full">
+              <input
+                type="submit"
+                value="Confirm"
+                className="text-left font-light border border-slate-400 mt-2 rounded-lg p-1 shadow-sm bg-slate-300 cursor-pointer"
+              />
+            </div>
+          </div>
+        </form>
       </div>
-      <hr className="py-2" />
-      {/* <img className="" src="/nassau.jpeg" /> */}
-      {/* <Image /> */}
-      <ClickableImage source="nassau.jpeg" />
-      <div className="pl-2 divide-y divide-slate-300 space-y-2">
-        <div className={rowStyling}>
-          <Label title={"Name"} />
-          <input
-            type={"text"}
-            id="name"
-            {...register("name")}
-            className={inputStyling}
-          />
-        </div>
-        <div className={rowStyling + " flex-col"}>
-          <Label title={"Description"} />
-          <textarea
-            placeholder="A description for your NFT diploma"
-            rows={4}
-            cols={50}
-            id="name"
-            {...register("description")}
-            className={inputStyling + " p-2 mt-2 mr-2 text-sm ml-0"}
-          />
-        </div>
-        <div className="w-full">
-          <input
-            type="submit"
-            value="Confirm"
-            className="text-left font-light border border-slate-400 mt-2 rounded-lg p-1 shadow-sm bg-slate-300 cursor-pointer"
-          />
-        </div>
-      </div>
-    </form>
+    </>
   );
 };
